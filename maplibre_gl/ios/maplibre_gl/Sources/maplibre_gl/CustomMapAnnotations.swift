089d55849024bdf7ab93e35f08024121ab064e81
Difta Fitrahul Qihaj <difta.fitr@gmail.com>
2026-02-03 17:23:27 +0700

    feat: update

--- maplibre_gl/ios/maplibre_gl/Sources/maplibre_gl/CustomMapAnnotations.swift
+++ maplibre_gl/ios/maplibre_gl/Sources/maplibre_gl/CustomMapAnnotations.swift
@@ -1,6 +1,16 @@
 import MapLibre
 import UIKit
 
+class CustomAnnotation: NSObject, MLNAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String? = nil) {
+        self.coordinate = coordinate
+        self.title = title
+    }
+}
 
 class SimpleBouncingView: MLNAnnotationView {
     
